b4db2d8e968d76db401296e38feb3b928ba73675
AceMido <acemido2004@gmail.com>
2023-08-11 06:05:29 +0300

    task 0 test 3

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import add_0
-a = 1
-b = 2
-result = add_0.add(a,b)
-print(f"{a} + {b} = {result}")
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add_0.add(a,b)
+    print(f"{a} + {b} = {result}")
