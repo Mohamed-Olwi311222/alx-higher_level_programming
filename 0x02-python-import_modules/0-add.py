7f8cdc6d44cca1a70572e0795e82b874921af318
AceMido <acemido2004@gmail.com>
2023-08-11 06:09:10 +0300

    used add_0 only once

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import add_0
+from add_0 import add
 if __name__ == "__main__":
     a = 1
     b = 2
-    result = add_0.add(a, b)
+    result = add(a, b)
     print(f"{a} + {b} = {result}")
