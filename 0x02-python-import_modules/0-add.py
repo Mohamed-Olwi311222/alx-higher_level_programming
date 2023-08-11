c510c7ea5b1b2e28a25cecff75e6ee6162f11774
AceMido <acemido2004@gmail.com>
2023-08-11 05:59:55 +0300

    task 0 test 2, added only the function in add_0.py and passed a,b to it from 0-add.py

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 import add_0
-add_0.add()
+a = 1
+b = 2
+result = add_0.add(a,b)
+print(f"{a} + {b} = {result}")
--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,7 +1,2 @@
 def add(a, b):
     return a + b
-
-a = 1
-b = 2
-result = add(a,b)
-print(f"{a} + {b} = {result}")
