117342e279d512a8b5c664834bbf3dc6169ce9a8
AceMido <acemido2004@gmail.com>
2023-08-14 01:39:52 +0300

    swaped the conditions

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if not my_list:
-        return None
-    else:
+    if my_list:
         big = 0
         for num in my_list:
-            if big < num:
+            if big <= num:
                 big = num
-    return big
+        return big
+    else:
+        return None
