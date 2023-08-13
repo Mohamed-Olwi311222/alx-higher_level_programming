379c939050e3c84324f41ec684d814489da1c5ab
AceMido <acemido2004@gmail.com>
2023-08-14 01:44:38 +0300

    changed the returned vvar name and made it return None as a string

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if not my_list:
-        return None
+        return "None"
     else:
-        big = 0
+        max = 0
         for num in my_list:
-            if big <= num:
-                big = num
-    return big
+            if max <= num:
+                max = num
+    return max
