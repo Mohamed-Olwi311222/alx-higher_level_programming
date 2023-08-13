8dd71c9879e926f2ba564a2e092df47894aa627d
AceMido <acemido2004@gmail.com>
2023-08-14 01:35:42 +0300

    fixed the algorithm

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,8 @@ def max_integer(my_list=[]):
     if not my_list:
         return None
     else:
-        sum = 0
+        big = 0
         for num in my_list:
-            sum += num
-    return sum
+            if big < num:
+                big = num
+    return big
