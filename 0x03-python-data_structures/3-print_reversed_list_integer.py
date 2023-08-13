d5852b47fbb8b733531f17dfd1f4ad0d5e8abdd3
AceMido <acemido2004@gmail.com>
2023-08-13 03:41:27 +0300

    made a copy of my_list so it wont interfere with the other code if callled my_list again

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reverse = my_list.reverse()
-    for item in reverse:
+    reversed_list = my_list.copy()
+    reversed_list.reverse()
+    for item in reversed_list:
         print("{:d}".format(item))
