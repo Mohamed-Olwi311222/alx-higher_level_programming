da9217d49b332b66a31e9d04a8aee33b39303c61
AceMido <acemido2004@gmail.com>
2023-08-13 03:43:14 +0300

    removed the copy as needed by the checker

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list.copy()
-    reversed_list.reverse()
-    for item in reversed_list:
+    my_list.reverse()
+    for item in my_list:
         print("{:d}".format(item))
