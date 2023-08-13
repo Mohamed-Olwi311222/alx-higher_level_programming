63941e189d11a564f54fa6ed59f1d72ebd9288a3
AceMido <acemido2004@gmail.com>
2023-08-13 03:46:29 +0300

    added if statment to check if my_list isnt equal to none

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    reversed_list = my_list.copy()
-    reversed_list.reverse()
-    for item in reversed_list:
-        print("{:d}".format(item))
+    if my_list:
+        reversed_list = my_list.copy()
+        reversed_list.reverse()
+        for item in reversed_list:
+            print("{:d}".format(item))
