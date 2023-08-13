0e31af9008e386d80e2413627422b1e97e02f114
AceMido <acemido2004@gmail.com>
2023-08-14 01:40:51 +0300

    brought back the old conditions order

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
+        return None
+    else:
         big = 0
         for num in my_list:
             if big <= num:
                 big = num
-        return big
-    else:
-        return None
+    return big
