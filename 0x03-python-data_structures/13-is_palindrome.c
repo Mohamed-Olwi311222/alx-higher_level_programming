41cf44c7a321bde496cbeae896044af144f47cdc
AceMido <acemido2004@gmail.com>
2023-08-14 05:28:52 +0300

    t1

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+/**
+ * is_palindrome - checks if the list is pallindrome or not
+ * @head: head of the list
+ * Return: 1 if pallindrome or 0 or not
+*/
+int is_palindrome(listint_t **head)
+{
+    listint_t *copy = NULL;
+    listint_t *orig = *head;
+    listint_t *ptr = NULL;
+
+    copy = copyList(*head);
+    reverseList(&copy);
+    ptr = copy;
+    while (orig != NULL)
+    {
+        if (orig->n != ptr->n)
+        {
+            return (0);
+        }
+        ptr = ptr->next;
+        orig = orig->next;
+    }
+    free_listint(copy);
+    return (1);
+}
+/**
+ * copyList - copy a linked list
+ * @original: original list
+ * Return: the head of the new list
+*/
+listint_t *copyList(listint_t *original)
+{
+    listint_t *copy = NULL;
+
+    if (!original)
+    {
+        return (NULL);
+    }
+        while (original != NULL)
+        {
+            add_nodeint_end(&copy, original->n);
+            original = original->next;   
+        }
+    return (copy);
+}
+/**
+ * reverseList - reverse a linked list
+ * @head: head of the list to reverse
+*/
+void reverseList(listint_t **head)
+{
+    listint_t *prev = *head;
+    listint_t *curr = (*head)->next;
+    listint_t *next = NULL;
+
+    if (!head)
+        return;
+    while (curr != NULL)
+    {
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
+    }
+    *head = prev;
+}
--- 0x03-python-data_structures/lists.h
+++ 0x03-python-data_structures/lists.h
@@ -17,11 +17,11 @@ typedef struct listint_s
 	int n;
 	struct listint_s *next;
 } listint_t;
-
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint_end(listint_t **head, const int n);
 void free_listint(listint_t *head);
-
+listint_t *copyList(listint_t *original);
+void reverseList(listint_t **head);
 int is_palindrome(listint_t **head);
 
 #endif /* LISTS_H */
