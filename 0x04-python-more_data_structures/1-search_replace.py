7ecdda751cccf2e67027414ca0122a54ff66c1aa
AceMido <acemido2004@gmail.com>
2023-08-16 23:24:13 +0300

    fixed the for loop to change very element(equal to search) to the new desired one(replace)

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,8 +2,10 @@
 def search_replace(my_list, search, replace):
     new_list = []
     if my_list:
-        index = my_list.index(search)
         new_list.extend(my_list)
-        new_list.pop(index)
-        new_list.insert(index, replace)
+        for i, _ in enumerate(new_list):
+            if new_list[i] == search:
+                new_list.pop(i)
+                new_list.insert(i, replace)
+                
     return new_list
