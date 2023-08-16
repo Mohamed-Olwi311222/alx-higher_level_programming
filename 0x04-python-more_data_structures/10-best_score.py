1f6a4b62b5df23c6373a7ecea89bc72128556485
AceMido <acemido2004@gmail.com>
2023-08-17 01:53:56 +0300

    t10 test 1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,1 +1,8 @@
 #!/usr/bin/python3
+def best_score(a_dictionary):
+    for key, value in a_dictionary.items():
+        if best < value:
+            best = value
+    if best == 0:
+        return None
+    return best
