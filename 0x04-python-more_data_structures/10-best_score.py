38c868ea409b71a4be374ea4e40e4fdde2efeda3
AceMido <acemido2004@gmail.com>
2023-08-17 01:57:28 +0300

    added an if statement to handle none

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+
+    best = 0
     for key, value in a_dictionary.items():
         if best < value:
             best = value
-    if best == 0:
-        return None
     return best
