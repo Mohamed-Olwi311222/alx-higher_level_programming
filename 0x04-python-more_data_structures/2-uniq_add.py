faf862d034a59832dabfa23d1e4fc8637e587dbe
AceMido <acemido2004@gmail.com>
2023-08-17 00:31:26 +0300

    t1 test 1

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+def uniq_add(my_list=[]):
+    new_list = []
+    if my_list:
+        for num in my_list:
+            if num not in new_list:
+                new_list.append(num)
+    return sum(new_list)
+
