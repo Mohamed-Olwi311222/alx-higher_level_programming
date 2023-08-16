de90596b00028ae583a942fbb1557234867fb9f2
AceMido <acemido2004@gmail.com>
2023-08-16 23:10:05 +0300

    t0 test 1

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    if matrix:
+
+        for row in matrix:
+            squared_row = []
+            for num in row:
+                squared_row.append(num ** 2)
+
+            new_matrix.append(squared_row)
+
+    return new_matrix
