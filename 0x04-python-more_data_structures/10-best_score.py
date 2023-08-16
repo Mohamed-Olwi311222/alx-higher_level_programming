270dbf9ed65fdb61aa2d4ffd944ab057de85293e
AceMido <acemido2004@gmail.com>
2023-08-17 02:00:54 +0300

    returned the key instead of the value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or not a_dictionary:
         return None
 
-    best = 0
+    best_value = 0
+    best_key = None
     for key, value in a_dictionary.items():
-        if best < value:
-            best = value
-    return best
+        if best_value < value:
+            best_value = value
+            best_key = key
+    return best_key
