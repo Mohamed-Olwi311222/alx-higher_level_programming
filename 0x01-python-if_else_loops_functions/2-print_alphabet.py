ec2c01feb540c7d4ad1bdd5a62bdb97322ea4a5d
AceMido <acemido2004@gmail.com>
2023-08-30 03:12:18 +0300

    all python tasks

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,1 +1,3 @@
 #!/usr/bin/python3
+for current in range(97, 123):
+    print("{:s}".format(chr(current)), end="")
--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+for current in range(97, 123):
+    if current == 113 or current == 101:
+        continue
+    print("{:s}".format(chr(current)), end="")
