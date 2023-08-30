ef4fe93d4a595702f072bda5c410cdc804003731
AceMido <acemido2004@gmail.com>
2023-08-30 09:32:23 +0300

    opitimized the for loop

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -28,8 +28,7 @@ class Square:
             print()
         else:
             for i in range(self.__size):
-                for j in range(self.__size):
-                    print("#", end="")
+                print("#" * self.__size, end="")
                 print()
 
     @property
