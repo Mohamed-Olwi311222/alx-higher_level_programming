94ae30089963ea441f04b41de146f7c8a3643ec2
AceMido <acemido2004@gmail.com>
2023-08-30 09:34:11 +0300

    removed the underscore in the for loop

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -31,7 +31,7 @@ class Square:
             for _ in range(self.__position[1]):
                 print()
             for _ in range(self.__size):
-                print("_" * self.__position[0] + "#" * self.__size)
+                print(" " * self.__position[0] + "#" * self.__size)
 
     @property
     def size(self):
