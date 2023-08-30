92f0c0c3955ea9b219ef50598b7aa00d3f9f2149
AceMido <acemido2004@gmail.com>
2023-08-30 04:26:32 +0300

    fixed pycodestyle

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 
 """Define a class Square."""
+
+
 class Square:
     """Represent a square"""
     def __init__(self, size):
         """Intialize a new square
-        
+
         Args:
             size (int): the size of the new square
         """
