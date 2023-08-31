59f572c3d343831e147c0ad2151f0064100832ae
Mohamed Olwi Elbayoumy hesen Elshennawy <111067383+AceMido@users.noreply.github.com>
2023-08-31 04:55:47 +0300

    Delete 0x04-python-more_data_structures directory

--- 0x04-python-more_data_structures/5-number_keys.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-def number_keys(a_dictionary):
-    count = 0
-    if a_dictionary:
-        keys = a_dictionary.keys()
-        for count, _ in enumerate(keys):
-            count += 1
-    return count
