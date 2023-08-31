582c2a61fb49642031be484bc0cbbd81350655f2
Mohamed Olwi Elbayoumy hesen Elshennawy <111067383+AceMido@users.noreply.github.com>
2023-08-31 04:54:58 +0300

    Delete 0x02-python-import_modules directory

--- 0x02-python-import_modules/2-args.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-
-    print("{} arguments:".format(len(argv) - 1))
-    for i, item in enumerate(argv[1:], start=1):
-        print("{}: {}".format(i, argv[i]))
